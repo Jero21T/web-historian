6de3ed1d5e04206c91ffc9cd7421f65698e267b0
connorc1231 <connorc1231@gmail.com>
2017-12-20 14:35:48 -0800

    Complete Solo Work

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,25 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
+var requestMethods = {
+  "GET": function(req, res) {
+    fs.readFile(__dirname + '/public/index.html', 'utf-8', function(err, data) {
+      if (err) {
+        console.log('error ' + err)
+      } else {
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.end(data)
+      }
+    })
+  }
+
+}
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if(requestMethods[req.method]) {
+    requestMethods[req.method](req, res);
+  }
+
 };
